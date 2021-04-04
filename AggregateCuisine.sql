6e330b9ef9a7fdd14ad37f028f2361c78e9a84b6
Harry Karanikas <74354067+HarryKaranikas@users.noreply.github.com>
2021-04-05 01:09:34 +0300

    Delete AggregateCuisine.sql

--- AggregateCuisine.sql
+++
@@ -1,7 +0,0 @@
-SELECT cuisine_parent,count(order_id) as Frequency,
-ROUND(SUM(basket),2) as OrderValue,
-COUNT(DISTINCT (shop_id)) as unique_shops
-
-FROM `bi-2019-test.ad_hoc.orders_jan2021`
-GROUP BY 1
-ORDER BY  OrderValue DESC
